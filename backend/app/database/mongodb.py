b046e70fc02d95968dac0af06989b9210c906e5d
anshu-katiyar <katiyaranshu00@gmail.com>
2026-07-23 11:29:17 +0530

    Fix CORS

--- backend/app/database/mongodb.py
+++ backend/app/database/mongodb.py
@@ -3,13 +3,12 @@ from app.config import MONGODB_URL
 
 client = MongoClient(
     MONGODB_URL,
-    serverSelectionTimeoutMS=5000
+    tls=True,
+    retryWrites=True,
+    serverSelectionTimeoutMS=10000
 )
-
-
 
 db = client["ai_news_db"]
 
 news_collection = db["news_history"]
-
 users_collection = db["users"]
